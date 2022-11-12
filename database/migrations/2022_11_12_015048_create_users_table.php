b428211b05eff544c969e600dc7600c4b229ab70
knameless27 <knameless27@gmail.com>
2022-11-11 21:38:14 -0500

    avances

--- database/migrations/2022_11_12_015048_create_users_table.php
+++ database/migrations/2022_11_12_015048_create_users_table.php
@@ -15,11 +15,19 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('estado');
+
+            //relacion
+            $table->foreignId('rol_users_id')
+                ->constrained()
+                ->onUpdate('cascade')
+                ->onDelete('cascade');
+
             $table->rememberToken();
+            $table->softDeletes();
             $table->timestamps();
         });
     }
