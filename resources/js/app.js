75db3ee887d3dad0a8f34a349e4f0d8db4383bc7
knameless27 <knameless27@gmail.com>
2022-11-14 22:00:47 -0500

    avue implementado

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,7 @@
 require('./bootstrap');
+import 'bootstrap/dist/css/bootstrap.css';
+
+import { createApp } from "vue";
+
+createApp({})
+.component("otravez", require('./components/otravez.vue').default).mount("#app");
---
+++ resources/js/components/otravez.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <nav class="navbar fixed-top navbar-dark bg-dark">
+            <a class="navbar-brand" href="#">Navbar</a>
+
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#">Disabled</a>
+                </li>
+            </ul>
+            <form class="form-inline">
+                <input class="form-control" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+            </form>
+        </nav>
+    </div>
+</template>
+<style></style>
